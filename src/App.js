ca83fd3017739a07e3a00d9469fe9c69bd3fe637
gmarre <gauthier.marre40@gmail.com>
2023-10-19 19:31:39 +0200

    Add delete scenario and step with icon

--- src/App.js
+++ src/App.js
@@ -48,9 +48,25 @@ const App = () => {
     age: 0,
     taille: 0,
   });
-  
+
+
   };
+
+  const deleteScenario = (index) => {
+    console.log("index scenario : " + index)
+    const updatedScenarios = [...scenarios];
+    updatedScenarios.splice(index, 1);
+    setScenarios(updatedScenarios);
+  }
   
+  const deleteStep = (index,indexStep) => {
+    console.log("index scenario : " + index + " index step : " + indexStep)
+    console.log("deleteStep called");
+    const updatedScenarios = [...scenarios];
+    updatedScenarios[index].steps.splice(indexStep, 1);
+    setScenarios(updatedScenarios);
+  }
+
 
   const saveData = async () => {
     try {
@@ -108,15 +124,40 @@ const App = () => {
         <ul>
           {scenarios.map((scenario, index) => (
             <li key={index}>
-              {scenario.scenarioName},
-              <input type="submit" name="Delete" value="Delete Scenario" />
-              {/* faire le bouton on click delete */}
+              {scenario.scenarioName}
+              <button name="Delete" value="Delete Scenario" onClick={() => deleteScenario(index)}>
+                <svg
+                  className="delete-icon"
+                  viewBox="0 0 408.483 408.483"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g>
+                    <g>
+                      <path d="M87.748,388.784c0.461,11.01,9.521,19.699,20.539,19.699h191.911c11.018,0,20.078-8.689,20.539-19.699l13.705-289.316H74.043L87.748,388.784z M247.655,171.329c0-4.61,3.738-8.349,8.35-8.349h13.355c4.609,0,8.35,3.738,8.35,8.349v165.293c0,4.611-3.738,8.349-8.35,8.349h-13.355c-4.61,0-8.35-3.736-8.35-8.349V171.329z M189.216,171.329c0-4.61,3.738-8.349,8.349-8.349h13.355c4.609,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.737,8.349-8.349,8.349h-13.355c-4.61,0-8.349-3.736-8.349-8.349V171.329L189.216,171.329z M130.775,171.329c0-4.61,3.738-8.349,8.349-8.349h13.356c4.61,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.738,8.349-8.349,8.349h-13.356c-4.61,0-8.349-3.736-8.349-8.349V171.329z" />
+                      <path d="M343.567,21.043h-88.535V4.305c0-2.377-1.927-4.305-4.305-4.305H164.285c-2.377,0-4.304,1.928-4.304,4.305v16.737H64.916c-7.125,0-12.9,5.776-12.9,12.901V74.47h304.451V33.944C356.467,26.819,350.692,21.043,343.567,21.043z" />
+                    </g>
+                  </g>
+                </svg>
+                {/*<img src="\Users\MARRE\my-sheet-app\images\rubbish.svg" alt="Delete Scenario" className="delete-icon" />*/}
+              </button>
               <ul>
                 {scenario.steps.map((step, stepIndex) => (
                   <li key={stepIndex}>
-                    {step.stepName} - Âge: {step.age}, Taille: {step.taille}, 
-                    <input type="submit" name="Delete" value="Delete Step" />
-                    {/* faire le bouton on click pour delete*/}
+                    {step.stepName} - Âge: {step.age}, Taille: {step.taille} 
+                    <button name="Delete" value="Delete Step" onClick={() => deleteStep(index, stepIndex)}> 
+                      <svg
+                        className="delete-icon"
+                        viewBox="0 0 408.483 408.483"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <g>
+                          <g>
+                            <path d="M87.748,388.784c0.461,11.01,9.521,19.699,20.539,19.699h191.911c11.018,0,20.078-8.689,20.539-19.699l13.705-289.316H74.043L87.748,388.784z M247.655,171.329c0-4.61,3.738-8.349,8.35-8.349h13.355c4.609,0,8.35,3.738,8.35,8.349v165.293c0,4.611-3.738,8.349-8.35,8.349h-13.355c-4.61,0-8.35-3.736-8.35-8.349V171.329z M189.216,171.329c0-4.61,3.738-8.349,8.349-8.349h13.355c4.609,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.737,8.349-8.349,8.349h-13.355c-4.61,0-8.349-3.736-8.349-8.349V171.329L189.216,171.329z M130.775,171.329c0-4.61,3.738-8.349,8.349-8.349h13.356c4.61,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.738,8.349-8.349,8.349h-13.356c-4.61,0-8.349-3.736-8.349-8.349V171.329z" />
+                            <path d="M343.567,21.043h-88.535V4.305c0-2.377-1.927-4.305-4.305-4.305H164.285c-2.377,0-4.304,1.928-4.304,4.305v16.737H64.916c-7.125,0-12.9,5.776-12.9,12.901V74.47h304.451V33.944C356.467,26.819,350.692,21.043,343.567,21.043z" />
+                          </g>
+                        </g>
+                      </svg> 
+                    </button>
                   </li>
                 ))}
               </ul>
